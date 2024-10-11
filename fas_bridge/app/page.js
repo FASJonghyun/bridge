73768406b918000c22eb06b6e4a7a264441eb49a
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-11 11:09:18 +0900

    FEAT : [ 확인용 ]

--- fas_bridge/app/page.js
+++ fas_bridge/app/page.js
@@ -1,4 +1,4 @@
-"use client"; 
+"use client"; // 이 지시어를 맨 위에 추가하여 클라이언트 컴포넌트로 선언
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -10,44 +10,8 @@ export default function Home() {
 
   useEffect(() => {
     if (id) {
-      // 'og:title' 메타 태그 업데이트
-      const ogTitle = document.querySelector('meta[property="og:title"]');
-      if (ogTitle) {
-        ogTitle.setAttribute('content', `브릿지메타데이터 - ${id}`);
-      } else {
-        const newOgTitle = document.createElement('meta');
-        newOgTitle.setAttribute('property', 'og:title');
-        newOgTitle.setAttribute('content', `브릿지메타데이터 - ${id}`);
-        document.head.appendChild(newOgTitle);
-      }
-
-      // 'og:description' 메타 태그 업데이트
-      const ogDescription = document.querySelector('meta[property="og:description"]');
-      if (ogDescription) {
-        ogDescription.setAttribute(
-          'content',
-          '패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.'
-        );
-      } else {
-        const newOgDescription = document.createElement('meta');
-        newOgDescription.setAttribute('property', 'og:description');
-        newOgDescription.setAttribute(
-          'content',
-          '패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.'
-        );
-        document.head.appendChild(newOgDescription);
-      }
-
-      // 'og:url' 메타 태그 업데이트
-      const ogUrl = document.querySelector('meta[property="og:url"]');
-      if (ogUrl) {
-        ogUrl.setAttribute('content', `https://www.fashionandstyle.com/withdraw?id=${id}`);
-      } else {
-        const newOgUrl = document.createElement('meta');
-        newOgUrl.setAttribute('property', 'og:url');
-        newOgUrl.setAttribute('content', `https://www.fashionandstyle.com/withdraw?id=${id}`);
-        document.head.appendChild(newOgUrl);
-      }
+      console.log('#### CHECKING PARAM ON Client');
+      console.log(id);
     }
   }, [id]);
 
