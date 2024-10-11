d17f6c1533e67b284b9e3cfb04a706d4e649b1cd
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-11 10:22:00 +0900

    FEAT : [ 로직 수정 ]

--- fas_bridge/app/layout.js
+++ fas_bridge/app/layout.js
@@ -1,5 +1,5 @@
-import "./globals.css";
 import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,15 +12,36 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const metadata = {
+  title: "패션앤스타일 | Fashion & Style",
+  description:
+    "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
+
+  openGraph: {
+    type: "website",
+    url: "www.fashionandstyle.com",
+    title: "패션앤스타일 | Fashion & Style",
+    description:
+      "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
+    siteName: "패션앤스타일 (Fashion & Style)",
+    images: [],
+  },
+  robots: "index, follow",
+  other: {
+    "google-site-verification": "-vG60emgcOR17vO0Zmz0efJNHPBZt3BmR1cdQ-YG6nw",
+    copyright:
+      "Copyrights © 2024 패션앤스타일컴퍼니 주식회사 All Rights Reserved",
+    author: "패션앤스타일컴퍼니 주식회사",
+    keywords:
+      "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
+    date: new Date().toISOString(),
+    build: new Date().toISOString(),
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="This is the default description" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* 공통 메타 태그 */}
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
--- fas_bridge/app/page.js
+++ fas_bridge/app/page.js
@@ -1,4 +1,14 @@
 import BridgePage from "./bridge_page/BridgePage";
+import { makeMetadata } from "./utils/meta_data";
+
+export async function generateMetadata() {
+  return makeMetadata(
+    "회원탈퇴 | 패션앤스타일 (Fashion & Style)",
+    "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
+    "https://www.fashionandstyle.com/withdraw",
+  );
+};
+
 
 export default function Home() {
   return (
