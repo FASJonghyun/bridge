450c39fcf0eea154c48c5402d6d667456f2f72f1
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-11 09:36:18 +0900

    FEAT : [ 브릿지 페이지 렌딩 확인용 ]

---
+++ fas_bridge/app/bridge_page/BridgePage.js
@@ -0,0 +1,13 @@
+function BridgePage () {
+    return (
+        <>
+            <div id="BridgePage">
+                <h1>
+                    브릿지 페이지 인입 확인용
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default BridgePage;
--- fas_bridge/app/page.js
+++ fas_bridge/app/page.js
@@ -1,10 +1,9 @@
+import BridgePage from "./bridge_page/BridgePage";
 
 export default function Home() {
   return (
     <div id="Home">
-      <h1>
-        여긴 홈 입니다.
-      </h1>
+      <BridgePage/>
     </div>
   );
 }
