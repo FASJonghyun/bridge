ecc6763e096991375a6917d33fc0df341da4cd0d
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-11 11:07:28 +0900

    FEAT : [ 정적페이지 테스트 ]

--- fas_bridge/app/page.js
+++ fas_bridge/app/page.js
@@ -1,37 +1,55 @@
+"use client"; 
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import BridgePage from "./bridge_page/BridgePage";
-import { makeMetadata } from "./utils/meta_data";
 
-export async function getStaticPaths() {
-  // 예시로 미리 생성하고자 하는 ID 리스트를 정의
-  return {
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    fallback: false, // 정의된 경로만 빌드
-  };
-}
+export default function Home() {
+  const router = useRouter();
+  const { id } = router.query;
 
-export async function getStaticProps({ params }) {
-  const { id } = params;
+  useEffect(() => {
+    if (id) {
+      // 'og:title' 메타 태그 업데이트
+      const ogTitle = document.querySelector('meta[property="og:title"]');
+      if (ogTitle) {
+        ogTitle.setAttribute('content', `브릿지메타데이터 - ${id}`);
+      } else {
+        const newOgTitle = document.createElement('meta');
+        newOgTitle.setAttribute('property', 'og:title');
+        newOgTitle.setAttribute('content', `브릿지메타데이터 - ${id}`);
+        document.head.appendChild(newOgTitle);
+      }
 
-  return {
-    props: {
-      id,
-    },
-  };
-}
+      // 'og:description' 메타 태그 업데이트
+      const ogDescription = document.querySelector('meta[property="og:description"]');
+      if (ogDescription) {
+        ogDescription.setAttribute(
+          'content',
+          '패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.'
+        );
+      } else {
+        const newOgDescription = document.createElement('meta');
+        newOgDescription.setAttribute('property', 'og:description');
+        newOgDescription.setAttribute(
+          'content',
+          '패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.'
+        );
+        document.head.appendChild(newOgDescription);
+      }
 
-export async function generateMetadata({ params }) {
-  const { id } = params;
-
-  return makeMetadata(
-    `브릿지메타데이터 - ${id}`,
-    "패션앤스타일(Fashion & Style)이 제공하는 최신 트렌드 패션 아이템과 다양한 셀럽들의 스타일을 만나보세요.",
-    "https://www.fashionandstyle.com/withdraw",
-  );
-}
-
-export default function Home({ id }) {
-  console.log('#### CHECKING PARAM ON Client');
-  console.log(id);
+      // 'og:url' 메타 태그 업데이트
+      const ogUrl = document.querySelector('meta[property="og:url"]');
+      if (ogUrl) {
+        ogUrl.setAttribute('content', `https://www.fashionandstyle.com/withdraw?id=${id}`);
+      } else {
+        const newOgUrl = document.createElement('meta');
+        newOgUrl.setAttribute('property', 'og:url');
+        newOgUrl.setAttribute('content', `https://www.fashionandstyle.com/withdraw?id=${id}`);
+        document.head.appendChild(newOgUrl);
+      }
+    }
+  }, [id]);
 
   return (
     <div id="Home">
