857dd6d215a33a6cf3a816ba61d79e7ea86b8012
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-14 15:47:28 +0900

    FEAT : [ 카카오스토리 및 스레드 일때 로직 추가 ]

--- fas_bridge/app/bridge_page/BridgePage.js
+++ fas_bridge/app/bridge_page/BridgePage.js
@@ -65,61 +65,31 @@ function BridgePage({type,id}) {
   //   const inAppBrowserPattern = /kakaotalk|line|inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsapp|electron|wadiz|aliapp|zumapp|whale|kakaostory|band|twitter|daumapps|daumdevice\/mobile|fb_iab|fb4a|fban|fbios|fbss|trill|samsungbrowser\/[^1]/i;
 
   //   const externalUrl = 'https://www.fashionandstyle.com';
-
-  //   if (userAgent.match(/kakaotalk/i)) {
-  //     window.location.href = 'fashionandstyle://' + externalUrl.replace(/^https?:\/\//i, '');
-  //   } else if (userAgent.match(/line/i)) {
-  //     window.location.href = 'fashionandstyle://' + externalUrl.replace(/^https?:\/\//i, '');
-  //   } else if (document.referrer.includes('instagram.com')) {
-  //     if (/android/i.test(userAgent)) {
-  //       window.location.href = 'intent://' + externalUrl.replace(/^https?:\/\//i, '') + '#Intent;scheme=https;package=com.android.chrome;end';
-  //     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-  //       window.location.href = 'fashionandstyle://' + externalUrl.replace(/^https?:\/\//i, '');
-  //     } else {
-  //       window.location.href = externalUrl;
-  //     }
-  //     setTimeout(() => {
-  //       setShowModal(true);
-  //       window.location.replace(externalUrl);
-  //     }, 2000);
-  //   } else if (userAgent.match(inAppBrowserPattern)) {
-  //     if (/iphone|ipad|ipod/i.test(userAgent)) {
-  //       window.location.href = 'https://www.fashionandstyle.com/';
-  //       setTimeout(() => {
-  //         setShowModal(true);
-  //         window.location.replace("https://www.fashionandstyle.com");
-  //       }, 2000);
-  //     } else {
-  //       window.location.href = 'intent://' + targetUrl.replace(/https?:\/\//i, '') + '#Intent;scheme=https;package=com.android.chrome;end';
-  //     }
-  //   } else if (/android/i.test(userAgent)) {
-  //     window.location.href = 'https://www.fashionandstyle.com/';
-  //     setTimeout(() => {
-  //       setShowModal(true);
-  //     }, 2000);
-  //   } else if (/iphone|ipad|ipod/.test(userAgent) && !window.MSStream) {
-  //     window.location.href = 'https://www.fashionandstyle.com/';
-  //     setTimeout(() => {
-  //       setShowModal(true);
-  //     }, 2000);
-  //   }
-  // };
-
-  // const handlePageLoad = () => {
-  //   const userAgent = navigator.userAgent.toLowerCase();
-  //   const targetUrl = window.location.href;
-  //   const inAppBrowserPattern = /kakaotalk|line|inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsapp|electron|wadiz|aliapp|zumapp|whale|kakaostory|band|twitter|daumapps|daumdevice\/mobile|fb_iab|fb4a|fban|fbios|fbss|trill|samsungbrowser\/[^1]/i;
-
-  //   const externalUrl = 'https://www.fashionandstyle.com';
   //   const appScheme = `fashionandstyle://open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
 
+  //   // 페이스북 인앱 브라우저 감지
+  //   const isFacebookInAppBrowser = /fb_iab|fb4a|fban|fbios/.test(userAgent);
+
   //   if (userAgent.match(/kakaotalk/i)) {
   //     window.location.href = appScheme;
-  //   } else if (userAgent.match(/line/i)) {
+  //   } 
+  //   // 페이스북 인앱 브라우저 처리 추가
+  //   else if (isFacebookInAppBrowser) {
   //     window.location.href = appScheme;
-  //   } else if (document.referrer.includes('instagram.com')) {
+  //     setTimeout(() => {
+  //       setShowModal(true);
+  //       // window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       if (/android/i.test(userAgent)) {
+  //         window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //       } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //         // window.location.href = "https://apps.apple.com/app/id1620312420";
+  //         window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       }
+  //     }, 2000);
+  //   } 
+  //   else if (document.referrer.includes('instagram.com')) {
   //     if (/android/i.test(userAgent)) {
-  //       window.location.href = `intent://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
+  //       window.location.href = `fashionandstyle://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
   //     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
   //       window.location.href = appScheme;
   //     } else {
@@ -127,60 +97,138 @@ function BridgePage({type,id}) {
   //     }
   //     setTimeout(() => {
   //       setShowModal(true);
-  //       window.location.replace(externalUrl);
+  //       // if (/android/i.test(userAgent)) {
+  //       //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //       // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //       //   // window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       //   window.location.replace(`https://apps.apple.com/app/id1620312420/?type=${type}&id=${id}`);
+  //       // }
+  //       // timeOutMethod();
   //     }, 2000);
-  //   } else if (userAgent.match(inAppBrowserPattern)) {
-  //     if (/iphone|ipad|ipod/i.test(userAgent)) {
-  //       window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
-  //       setTimeout(() => {
-  //         setShowModal(true);
-  //         window.location.replace(externalUrl);
-  //       }, 2000);
+  //   } 
+  //   else if (userAgent.match(inAppBrowserPattern)) {
+  //     // if (/iphone|ipad|ipod/i.test(userAgent)) {
+  //     //   window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
+  //     //   setTimeout(() => {
+  //     //     setShowModal(true);
+  //     //     // window.location.replace(externalUrl);
+  //     //     // if (/android/i.test(userAgent)) {
+  //     //     //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //     //     // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //     //     //   window.location.replace("https://apps.apple.com/app/id1620312420");
+  //     //     // }
+  //     //   }, 2000);
+  //     // } else {
+  //     //   window.location.href = `fashionandstyle://${targetUrl.replace(/https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
+  //     //   setTimeout(() => {
+  //     //     setShowModal(true);
+  //     //     // window.location.replace(externalUrl);
+  //     //     // if (/android/i.test(userAgent)) {
+  //     //     //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //     //     // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //     //     //   window.location.replace("https://apps.apple.com/app/id1620312420");
+  //     //     // }
+  //     //   }, 2000);
+  //     // }
+  //     if (/android/i.test(userAgent)) {
+  //       window.location.href = `fashionandstyle://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
+  //     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+  //       window.location.href = appScheme;
   //     } else {
-  //       window.location.href = `intent://${targetUrl.replace(/https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
+  //       window.location.href = externalUrl;
   //     }
-  //   } else if (/android/i.test(userAgent)) {
-  //     window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
   //     setTimeout(() => {
   //       setShowModal(true);
+  //       // if (/android/i.test(userAgent)) {
+  //       //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //       // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //       //   // window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       //   window.location.replace(`https://apps.apple.com/app/id1620312420/?type=${type}&id=${id}`);
+  //       // }
+  //       // timeOutMethod();
   //     }, 2000);
-  //   } else if (/iphone|ipad|ipod/.test(userAgent) && !window.MSStream) {
+  //   } 
+  //   else if (/android/i.test(userAgent)) {
   //     window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
   //     setTimeout(() => {
   //       setShowModal(true);
+  //       // setTimeout(() => {
+  //       //   setShowModal(true);
+  //       //   // window.location.replace(externalUrl);
+  //       //   if (/android/i.test(userAgent)) {
+  //       //     window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //       //   } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //       //     window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       //   }
+  //       // }, 2000);
+  //     }, 2000);
+  //   } 
+  //   else if (/iphone|ipad|ipod/.test(userAgent) && !window.MSStream) {
+  //     window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
+  //     setTimeout(() => {
+  //       setShowModal(true);
+  //       // setTimeout(() => {
+  //       //   setShowModal(true);
+  //       //   // window.location.replace(externalUrl);
+  //       //   if (/android/i.test(userAgent)) {
+  //       //     window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
+  //       //   } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
+  //       //     // window.location.href = "https://apps.apple.com/app/id1620312420";
+  //       //     window.location.replace("https://apps.apple.com/app/id1620312420");
+  //       //   }
+  //       // }, 2000);
   //     }, 2000);
   //   }
   // };
 
+  ///
   const handlePageLoad = () => {
     const userAgent = navigator.userAgent.toLowerCase();
     const targetUrl = window.location.href;
-    const inAppBrowserPattern = /kakaotalk|line|inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsapp|electron|wadiz|aliapp|zumapp|whale|kakaostory|band|twitter|daumapps|daumdevice\/mobile|fb_iab|fb4a|fban|fbios|fbss|trill|samsungbrowser\/[^1]/i;
-
     const externalUrl = 'https://www.fashionandstyle.com';
     const appScheme = `fashionandstyle://open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
-
+  
+    // inAppBrowserPattern에서 'kakaostory' 제거
+    const inAppBrowserPattern = /kakaotalk|line|inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsapp|electron|wadiz|aliapp|zumapp|whale|band|twitter|daumapps|daumdevice\/mobile|fb_iab|fb4a|fban|fbios|fbss|trill|samsungbrowser\/[^1]/i;
+  
     // 페이스북 인앱 브라우저 감지
     const isFacebookInAppBrowser = /fb_iab|fb4a|fban|fbios/.test(userAgent);
-
+  
     if (userAgent.match(/kakaotalk/i)) {
+      // 카카오톡에서 앱 스킴으로 리디렉션
       window.location.href = appScheme;
     } 
-    // 페이스북 인앱 브라우저 처리 추가
+    else if (userAgent.match(/kakaostory/i)) {
+      // 카카오스토리에서 앱 스킴으로 리디렉션
+      window.location.href = appScheme;
+    }
+    else if (userAgent.match(/threads/i) || document.referrer.includes('threads.net')) {
+      // Threads에서 인스타그램과 동일한 로직 적용
+      if (/android/i.test(userAgent)) {
+        window.location.href = `fashionandstyle://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
+      } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+        window.location.href = appScheme;
+      } else {
+        window.location.href = externalUrl;
+      }
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2000);
+    }
     else if (isFacebookInAppBrowser) {
+      // 페이스북 인앱 브라우저 처리
       window.location.href = appScheme;
       setTimeout(() => {
         setShowModal(true);
-        // window.location.replace("https://apps.apple.com/app/id1620312420");
         if (/android/i.test(userAgent)) {
           window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
         } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-          // window.location.href = "https://apps.apple.com/app/id1620312420";
           window.location.replace("https://apps.apple.com/app/id1620312420");
         }
       }, 2000);
     } 
     else if (document.referrer.includes('instagram.com')) {
+      // 인스타그램에서 처리
       if (/android/i.test(userAgent)) {
         window.location.href = `fashionandstyle://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
       } else if (/iphone|ipad|ipod/i.test(userAgent)) {
@@ -190,39 +238,10 @@ function BridgePage({type,id}) {
       }
       setTimeout(() => {
         setShowModal(true);
-        // if (/android/i.test(userAgent)) {
-        //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-        // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-        //   // window.location.replace("https://apps.apple.com/app/id1620312420");
-        //   window.location.replace(`https://apps.apple.com/app/id1620312420/?type=${type}&id=${id}`);
-        // }
-        // timeOutMethod();
       }, 2000);
-    } 
+    }
     else if (userAgent.match(inAppBrowserPattern)) {
-      // if (/iphone|ipad|ipod/i.test(userAgent)) {
-      //   window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
-      //   setTimeout(() => {
-      //     setShowModal(true);
-      //     // window.location.replace(externalUrl);
-      //     // if (/android/i.test(userAgent)) {
-      //     //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-      //     // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-      //     //   window.location.replace("https://apps.apple.com/app/id1620312420");
-      //     // }
-      //   }, 2000);
-      // } else {
-      //   window.location.href = `fashionandstyle://${targetUrl.replace(/https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
-      //   setTimeout(() => {
-      //     setShowModal(true);
-      //     // window.location.replace(externalUrl);
-      //     // if (/android/i.test(userAgent)) {
-      //     //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-      //     // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-      //     //   window.location.replace("https://apps.apple.com/app/id1620312420");
-      //     // }
-      //   }, 2000);
-      // }
+      // 기타 인앱 브라우저에서 처리
       if (/android/i.test(userAgent)) {
         window.location.href = `fashionandstyle://${externalUrl.replace(/^https?:\/\//i, '')}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}#Intent;scheme=https;package=com.android.chrome;end`;
       } else if (/iphone|ipad|ipod/i.test(userAgent)) {
@@ -232,47 +251,27 @@ function BridgePage({type,id}) {
       }
       setTimeout(() => {
         setShowModal(true);
-        // if (/android/i.test(userAgent)) {
-        //   window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-        // } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-        //   // window.location.replace("https://apps.apple.com/app/id1620312420");
-        //   window.location.replace(`https://apps.apple.com/app/id1620312420/?type=${type}&id=${id}`);
-        // }
-        // timeOutMethod();
       }, 2000);
     } 
     else if (/android/i.test(userAgent)) {
+      // 일반 안드로이드 브라우저에서 처리
       window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
       setTimeout(() => {
         setShowModal(true);
-        // setTimeout(() => {
-        //   setShowModal(true);
-        //   // window.location.replace(externalUrl);
-        //   if (/android/i.test(userAgent)) {
-        //     window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-        //   } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-        //     window.location.replace("https://apps.apple.com/app/id1620312420");
-        //   }
-        // }, 2000);
       }, 2000);
     } 
     else if (/iphone|ipad|ipod/.test(userAgent) && !window.MSStream) {
+      // 일반 iOS 브라우저에서 처리
       window.location.href = `${externalUrl}/open?type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
       setTimeout(() => {
         setShowModal(true);
-        // setTimeout(() => {
-        //   setShowModal(true);
-        //   // window.location.replace(externalUrl);
-        //   if (/android/i.test(userAgent)) {
-        //     window.location.replace("https://play.google.com/store/apps/details?id=com.fas.android");
-        //   } else if (/iphone|ipad|ipod/i.test(userAgent) && !window.MSStream) {
-        //     // window.location.href = "https://apps.apple.com/app/id1620312420";
-        //     window.location.replace("https://apps.apple.com/app/id1620312420");
-        //   }
-        // }, 2000);
       }, 2000);
     }
   };
+  
+
+
+  ///
 
   // const handleStoreRedirect = () => {
   //   const userAgent = navigator.userAgent || navigator.vendor || window.opera;
